ed662b03751ab38de6ac565057e157cc470ed449
zengwanping997 <102908802+zengwanping997@users.noreply.github.com>
2025-06-17 18:27:21 +0800

    Update main_demo.py

--- 2024103481曾婉萍/作业三/main_demo.py
+++ 2024103481曾婉萍/作业三/main_demo.py
@@ -1,1 +1,23 @@
+from data_sampler import DataSampler
+from stat_analyzer import analyze
+
+# 1 结构定义
+structure = {
+    "id": int,
+    "score": float,
+    "name": str,
+    "active": bool
+}
+
+# 2 生成数据
+sampler = DataSampler(structure)
+data = sampler.sample(20)
+
+# 3 分析
+scores = [record["score"] for record in data]
+print("原始 scores：", scores)
+
+# print结果
+result = analyze(scores)
+print("统计分析结果：", result)
 
