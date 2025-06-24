6d52486305b70576a6e3e9f0059be0a8bfc948c3
wlj290 <3425662195@qq.com>
2025-06-24 13:38:14 +0800

    Add files via upload

---
+++ 2024103412李晓伟/项目/data_loader.py
@@ -0,0 +1,47 @@
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+def load_cifar10(batch_size=32, data_augmentation=True):
+    # 定义数据预处理
+    if data_augmentation:
+        train_transform = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(15),
+            transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+    else:
+        train_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+    
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    
+    # 加载CIFAR-10数据集
+    train_dataset = datasets.CIFAR10(
+        root='./data', train=True, download=True, transform=train_transform
+    )
+    test_dataset = datasets.CIFAR10(
+        root='./data', train=False, download=True, transform=test_transform
+    )
+    
+    # 创建数据加载器
+    train_loader = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True, num_workers=2
+    )
+    test_loader = DataLoader(
+        test_dataset, batch_size=batch_size, shuffle=False, num_workers=2
+    )
+    
+    # 类别名称
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 
+               'dog', 'frog', 'horse', 'ship', 'truck')
+    
+    return train_loader, test_loader, classes    
